6168fba952e0fc65fb032cb61a09ba58a5e55c6c
ashok <ashokprajapati999@gmail.com>
2019-02-26 09:23:10 +0530

    removed project

--- UserManagement/src/main/java/com/akp/usermgmt/service/UserMgmtService.java
+++
@@ -1,9 +0,0 @@
-package com.akp.usermgmt.service;
-
-import com.akp.usermgmt.domain.DemoUser;
-
-public interface UserMgmtService {
-	public void saveUser(DemoUser user);
-	public DemoUser findByUserId(String userId);
-	public void deleteUser(String userId);
-}
